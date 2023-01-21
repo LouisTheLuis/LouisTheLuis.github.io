38799c9ea01e24ef014f6cbc01c4981c0e6fa58d
Louis <luismapena15@gmail.com>
2023-01-21 18:26:55 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61ca7df5f9b4fd008df9571b3901798e.js"
+  "/precache-manifest.1657ff2f5918460a2541a61b0b87c781.js"
 );
 
 workbox.clientsClaim();
