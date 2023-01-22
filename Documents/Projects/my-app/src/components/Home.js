551625f147e9d2eb93a44167b8386754a261c6d2
Louis <luismapena15@gmail.com>
2023-01-21 19:18:44 -0500

    sigh

--- Documents/Projects/my-app/src/components/Home.js
+++ Documents/Projects/my-app/src/components/Home.js
@@ -7,40 +7,71 @@ import {
   AiFillLinkedin,
 } from "react-icons/ai";
 
-const Home = () => {
+const LoadingComponent = () => <div style={{width:"100vw", height:"100vh", justifyContent:"center", alignItems:"center", display:"flex"}}>
+  <div class="lds-dual-ring"></div>
+</div>;
 
-  return (
-    <>
-      <Navbar/>
-      <div className="home-square">
-        <div className="text-square">
-          Hi! I'm Louis, a 3rd year Electrical Engineering and Computer Science student at MIT
-          <div className="icon-square">
-            <a href="https://github.com/LouisTheLuis" target="_blank" rel="noreferrer" style={{color:"#902D41"}}><AiFillGithub/></a>
-            <a href="luism@mit.edu" target="_blank" rel="noreferrer" style={{color:"#902D41"}}><AiFillMail/></a>
-            <a href="https://www.linkedin.com/in/luis-martinez-a55285250/" target="_blank" rel="noreferrer" style={{color:"#902D41"}}><AiFillLinkedin/></a>
+const Home = () => {
+  const [mobile, setMobile] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const onLoadEffect = () => {
+      setTimeout(() => {
+          setLoading(false);
+      }, 700);
+  };
+  useEffect(onLoadEffect, []);
+
+  useEffect(() => {
+      const temp = window.innerWidth <= 767;
+      setMobile(temp);
+      console.log(window.innerWidth);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingComponent />;
+  }
+
+  if (!mobile) {
+    return (
+      <>
+        <Navbar/>
+        <div className="home-square">
+          <div className="text-square">
+            Hi! I'm Louis, a 3rd year Electrical Engineering and Computer Science student at MIT
+            <div className="icon-square">
+              <a href="https://github.com/LouisTheLuis" target="_blank" rel="noreferrer" style={{color:"#902D41"}}><AiFillGithub/></a>
+              <a href="luism@mit.edu" target="_blank" rel="noreferrer" style={{color:"#902D41"}}><AiFillMail/></a>
+              <a href="https://www.linkedin.com/in/luis-martinez-a55285250/" target="_blank" rel="noreferrer" style={{color:"#902D41"}}><AiFillLinkedin/></a>
+            </div>
+          </div>
+          <div className="text-square">
+            <div className="image-square"><img src={require('../assets/yo.jpg').default} className="resize"/></div>
           </div>
         </div>
-        <div className="text-square">
-          <div className="image-square"><img src={require('../assets/yo.jpg').default} className="resize"/></div>
+        <div className="home-square">
+          <div className="text-square">
+            <div className="image-square"><img src={require('../assets/osci.jpg').default} className="resize"/></div>
+          </div>
+          <div className="text-square">
+            I love hardware, software, and most importantly, making stuff! Check my project page!
+          </div>
         </div>
-      </div>
-      <div className="home-square">
-        <div className="text-square">
-          <div className="image-square"><img src={require('../assets/osci.jpg').default} className="resize"/></div>
+        <div className="home-square">
+          <div className="text-square">I am from Puerto Ordaz, Venezuela. During my free time, I love reading, appreciating film, and learning about history!</div>
+          <div className="text-square">
+            <div className="image-square"><img src={require('../assets/books.jpg').default} className="resize"/></div>
+          </div>
         </div>
-        <div className="text-square">
-          I love hardware, software, and most importantly, making stuff! Check my project page!
-        </div>
-      </div>
-      <div className="home-square">
-        <div className="text-square">I am from Puerto Ordaz, Venezuela. During my free time, I love reading, appreciating film, and learning about history!</div>
-        <div className="text-square">
-          <div className="image-square"><img src={require('../assets/books.jpg').default} className="resize"/></div>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+      return (
+      <>
+        {/* <Navbar/> */}
+        <div style={{position:"relative", topMargin:"70px"}}>Phone not supported yet! :&#40;</div>
+      </>
+      );
+  }
 };
 
 export default Home;
--- Documents/Projects/my-app/src/components/Projects.js
+++ Documents/Projects/my-app/src/components/Projects.js
@@ -12,68 +12,98 @@ import {
     AiOutlineLink
   } from "react-icons/ai";
 
-const Projects = () => {
+const LoadingComponent = () => <div style={{width:"100vw", height:"100vh", justifyContent:"center", alignItems:"center", display:"flex"}}>
+    <div class="lds-dual-ring"></div>
+</div>;
 
-  return (
-    <>
-        <Navbar/>
-        <div className="projects-font">
-            <h1 style={{fontSize:"6vh", color:"#902D41", letterSpacing: "0.1em"}}>RECENT WORKS</h1>
-            <h2 style={{color:"#b56776"}}>Here are some of the recent projects I have made!</h2>
-            <br/>
-            <div className="projects-square">
-                <div className="project-section" style={{boxShadow:"3px 3px 7px 2px rgba(190, 76, 99, 0.6)"}}>
-                    <h1 style={{fontSize:"4vh", opacity:"0.5"}}>Audio Transmission via S/PDIF</h1>
-                    <img src={spdif} style={{width:"25vw"}}/>
-                    <div style={{width:"25vw", fontSize:"2vh", marginTop:"2vh", opacity:"0.7"}}>
-                        An interesting audio player! The project consisted in two Nexys 4 DDR FPGAs connected
-                        by a TOSLINK fiber optic cable. The transmitter FPGA would contain an SD card —which would itself
-                        have an implementation of the FAT12 file system in it— with .wav files, that would be selected
-                        through a VGA screen. Then these audio files would be retransmitted through a TOSLINK fiber optic cable
-                        using the S/PDIF audio transmission protocol to be received in a receiver FPGA, which would decode and reconstruct
-                        the signal to be sent to an audio speaker. My main responsibility lied on the implementation of the audio transmission protocol
-                        and the VGA screen.
+const Projects = () => {
+    const [mobile, setMobile] = useState(false);
+    const [isLoading, setLoading] = useState(true);
+    const onLoadEffect = () => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 700);
+    };
+    useEffect(onLoadEffect, []);
+
+    useEffect(() => {
+        const temp = window.innerWidth <= 767;
+        setMobile(temp);
+        console.log(window.innerWidth);
+    }, []);
+
+    if (isLoading) {
+        return <LoadingComponent />;
+      }
+
+    if (!mobile) {
+        return (
+        <>
+            <Navbar/>
+            <div className="projects-font">
+                <h1 style={{fontSize:"6vh", color:"#902D41", letterSpacing: "0.1em"}}>RECENT WORKS</h1>
+                <h2 style={{color:"#b56776"}}>Here are some of the recent projects I have made!</h2>
+                <br/>
+                <div className="projects-square">
+                    <div className="project-section" style={{boxShadow:"3px 3px 7px 2px rgba(190, 76, 99, 0.6)"}}>
+                        <h1 style={{fontSize:"4vh", opacity:"0.5"}}>Audio Transmission via S/PDIF</h1>
+                        <img src={spdif} style={{width:"25vw"}}/>
+                        <div style={{width:"25vw", fontSize:"2vh", marginTop:"2vh", opacity:"0.7"}}>
+                            An interesting audio player! The project consisted in two Nexys 4 DDR FPGAs connected
+                            by a TOSLINK fiber optic cable. The transmitter FPGA would contain an SD card —which would itself
+                            have an implementation of the FAT12 file system in it— with .wav files, that would be selected
+                            through a VGA screen. Then these audio files would be retransmitted through a TOSLINK fiber optic cable
+                            using the S/PDIF audio transmission protocol to be received in a receiver FPGA, which would decode and reconstruct
+                            the signal to be sent to an audio speaker. My main responsibility lied on the implementation of the audio transmission protocol
+                            and the VGA screen.
+                        </div>
+                        <div style={{display:"flex", flexDirection:"row", gap:"8vw", marginTop:"2vh"}}>
+                            <a href="https://github.com/alkeirn/optical_project" target="_blank" style={{textDecoration:"none"}}><button className="project-buttons"><AiFillGithub style={{fontSize:"3vh"}}/> Github</button></a>
+                            <a href={spdifreport} target="_blank" style={{textDecoration:"none"}}><button className="project-buttons"><AiOutlineFilePdf style={{fontSize:"3vh"}}/> Final Report</button></a>
+                        </div>
                     </div>
-                    <div style={{display:"flex", flexDirection:"row", gap:"8vw", marginTop:"2vh"}}>
-                        <a href="https://github.com/alkeirn/optical_project" target="_blank" style={{textDecoration:"none"}}><button className="project-buttons"><AiFillGithub style={{fontSize:"3vh"}}/> Github</button></a>
-                        <a href={spdifreport} target="_blank" style={{textDecoration:"none"}}><button className="project-buttons"><AiOutlineFilePdf style={{fontSize:"3vh"}}/> Final Report</button></a>
+                    <div className="project-section" style={{boxShadow:"0px 3px 7px 2px rgba(190, 76, 99, 0.6)"}}>
+                        <h1 style={{fontSize:"4vh", opacity:"0.5"}}>Stargazer</h1>
+                        <img src={stargazer} style={{width:"27vw"}}/>
+                        <div style={{width:"25vw", fontSize:"2vh", marginTop:"2vh", opacity:"0.7"}}>
+                            An interactive game designed for the WebLab MIT competition, in collaboration with a classmate.
+                            Through an implementation of the d3-celestial API developed from the D3.js visualization library, we designed
+                            a game that allowed the user to learn and practice searching constellations in the sky. In order to make this possible,
+                            we made use of the React.js framework. 
+                            <br/>
+                            The functionality of the website allows for a small encyclopedia that encompasses the original 
+                            48 Greek constellations, a casual game mode, a competitive mode, and a leaderboard.
+                        </div>
+                        <div style={{display:"flex", flexDirection:"row", gap:"8vw", marginTop:"2vh"}}>
+                            <a href="https://github.com/LouisTheLuis/the-stargazer" target="_blank" style={{textDecoration:"none"}}><button className="project-buttons"><AiFillGithub style={{fontSize:"3vh"}}/> Github</button></a>
+                        </div>
                     </div>
-                </div>
-                <div className="project-section" style={{boxShadow:"0px 3px 7px 2px rgba(190, 76, 99, 0.6)"}}>
-                    <h1 style={{fontSize:"4vh", opacity:"0.5"}}>Stargazer</h1>
-                    <img src={stargazer} style={{width:"27vw"}}/>
-                    <div style={{width:"25vw", fontSize:"2vh", marginTop:"2vh", opacity:"0.7"}}>
-                        An interactive game designed for the WebLab MIT competition, in collaboration with a classmate.
-                        Through an implementation of the d3-celestial API developed from the D3.js visualization library, we designed
-                        a game that allowed the user to learn and practice searching constellations in the sky. In order to make this possible,
-                        we made use of the React.js framework. 
-                        <br/>
-                        The functionality of the website allows for a small encyclopedia that encompasses the original 
-                        48 Greek constellations, a casual game mode, a competitive mode, and a leaderboard.
-                    </div>
-                    <div style={{display:"flex", flexDirection:"row", gap:"8vw", marginTop:"2vh"}}>
-                        <a href="https://github.com/LouisTheLuis/the-stargazer" target="_blank" style={{textDecoration:"none"}}><button className="project-buttons"><AiFillGithub style={{fontSize:"3vh"}}/> Github</button></a>
-                    </div>
-                </div>
-                <div className="project-section" style={{boxShadow:"-3px 3px 7px 2px rgba(190, 76, 99, 0.6)"}}>
-                    <h1 style={{fontSize:"4vh", opacity:"0.5"}}>Broadband Amplifier</h1>
-                    <img src={broadband} style={{width:"20vw"}}/>
-                    <div style={{width:"25vw", fontSize:"2vh", marginTop:"2vh", opacity:"0.7"}}>
-                        Final project for my Solid-State Circuits class. Basically a broadband amplifier that achieves a series of benchmarks: a midband
-                        gain greater than 200, an upper 3dB bandwith frequency <i>f<sub>h</sub></i>greater than 5MHz, 
-                        a lower 3dB bandwith frequency <i>f<sub>h</sub></i>smaller than 100kHz, and a power dissipation of less than 100mW!
-                        <br/>
-                        The design was made on LTSpice and tested on a breadboard, using a total of 3 2N3904 npn transistors for the design. The specs were
-                        verified with an oscilloscope.
-                    </div>
-                    <div style={{display:"flex", flexDirection:"row", gap:"8vw", marginTop:"3vh"}}>
-                        <a href={broadbandreport} target="_blank" style={{textDecoration:"none"}}><button className="project-buttons"><AiOutlineFilePdf style={{fontSize:"3vh"}}/>Final Report</button></a>
+                    <div className="project-section" style={{boxShadow:"-3px 3px 7px 2px rgba(190, 76, 99, 0.6)"}}>
+                        <h1 style={{fontSize:"4vh", opacity:"0.5"}}>Broadband Amplifier</h1>
+                        <img src={broadband} style={{width:"20vw"}}/>
+                        <div style={{width:"25vw", fontSize:"2vh", marginTop:"2vh", opacity:"0.7"}}>
+                            Final project for my Solid-State Circuits class. Basically a broadband amplifier that achieves a series of benchmarks: a midband
+                            gain greater than 200, an upper 3dB bandwith frequency <i>f<sub>h</sub></i>greater than 5MHz, 
+                            a lower 3dB bandwith frequency <i>f<sub>h</sub></i>smaller than 100kHz, and a power dissipation of less than 100mW!
+                            <br/>
+                            The design was made on LTSpice and tested on a breadboard, using a total of 3 2N3904 npn transistors for the design. The specs were
+                            verified with an oscilloscope.
+                        </div>
+                        <div style={{display:"flex", flexDirection:"row", gap:"8vw", marginTop:"3vh"}}>
+                            <a href={broadbandreport} target="_blank" style={{textDecoration:"none"}}><button className="project-buttons"><AiOutlineFilePdf style={{fontSize:"3vh"}}/>Final Report</button></a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-  );
+        </>
+        );
+    } else {
+        return (
+        <>
+            <Navbar/>
+        </>
+        );
+    }
 };
 
 export default Projects;
